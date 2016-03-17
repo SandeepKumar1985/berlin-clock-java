a391d53c277e80ff21ce8e3ea1d5375d1de3e515
Ryan Burke <ryan.burke@digital.hmrc.gov.uk>
2016-03-17 16:34:58 +0000

    Removed BerlinTime, refactored BerlinClock, added comments

--- src/main/java/BerlinClock.java
+++ src/main/java/BerlinClock.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * Berlin Clock.
+ *
+ * Creates a representation of a berlin clock from a String and provides
+ * a way of printing this.
+ */
 public class BerlinClock {
 
-    private BerlinTime berlinTime;
+    private String formattedTime;
+    private static final String NEW_LINE = System.getProperty("line.separator");
 
+    /**
+     * Create a new BerlinClock instance with a string representing time.
+     *
+     * @param time - The 24 hour time in the format of HH:MM:SS
+     */
     public BerlinClock(String time) {
 
         if(time == null) throw new IllegalArgumentException("No time provided.");
@@ -20,14 +33,19 @@ public class BerlinClock {
         if (minutes < 0 || minutes > 59) throw new IllegalArgumentException("Minutes out of bounds.");
         if (seconds < 0 || seconds > 59) throw new IllegalArgumentException("Seconds out of bounds.");
 
-        this.berlinTime = processTime(hours, minutes, seconds);
+        this.formattedTime = processTime(hours, minutes, seconds);
     }
 
-    public void printClock() {
-        System.out.println(berlinTime);
-    }
-
-    private BerlinTime processTime(int hours, int minutes, int seconds) {
+    /**
+     * Convert individual hours, minutes and seconds into a BerlinTime object.
+     *
+     * @param hours - an int representing Hours
+     * @param minutes - an int representing Minutes
+     * @param seconds - an int representing Seconds
+     *
+     * @return BerlinTime object created using the parameters.
+     */
+    private String processTime(int hours, int minutes, int seconds) {
 
         String line1 = (seconds % 2 == 0) ? "Y" : "0";
         String line2 = rowString(hours / 5, 4, "R");
@@ -35,9 +53,17 @@ public class BerlinClock {
         String line4 = rowString(minutes / 5, 11, "Y").replaceAll("YYY", "YYR");
         String line5 = rowString(minutes % 5, 4, "Y");
 
-        return new BerlinTime(line1, line2, line3, line4, line5);
+        return String.join(NEW_LINE, Arrays.asList(line1, line2, line3, line4, line5));
+
     }
 
+    /**
+     * Creates a string for each row of the berlin clock.
+     * @param litLights
+     * @param lightsInRow
+     * @param lampType
+     * @returnn A string representing a single row of the clock.
+     */
     private String rowString(int litLights, int lightsInRow, String lampType) {
 
         int unlitLights = lightsInRow - litLights;
@@ -45,6 +71,18 @@ public class BerlinClock {
         String unlit = String.join("", Collections.nCopies(unlitLights, "0"));
 
         return lit + unlit;
+    }
+
+    /**
+     * Print a representation of the berlin time.
+     */
+    public void printClock() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return this.formattedTime;
     }
 
 
